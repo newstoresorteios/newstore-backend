554e486d766baf310d3a00c362ca9e54ecef6988
Tironi <tironinho@hotmail.com>
2025-09-15 12:36:32 -0300

    Nova versao v1 release2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -207,7 +207,7 @@ router.post('/reset-password', async (req, res) => {
     });
 
     const mail = {
-      from: 'contato@newstorerj.com.br',
+      from: 'tironinho@hotmail.com',
       to: email,
       subject: 'Reset de senha - New Store Sorteios',
       text:
