1bdeb0ebf43d15b98880784cb06ffa036b70b02f
Tironi <tironinho@hotmail.com>
2025-09-15 12:25:22 -0300

    Nova versao v1 release2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -207,7 +207,7 @@ router.post('/reset-password', async (req, res) => {
     });
 
     const mail = {
-      from: 'administracao@newstoresorteios.com.br',
+      from: 'newrecreio@gmail.com',
       to: email,
       subject: 'Reset de senha - New Store Sorteios',
       text:
