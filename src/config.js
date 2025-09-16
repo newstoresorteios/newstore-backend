6740b5421469394332d1a072807596ae91506ed1
Tironi <tironinho@hotmail.com>
2025-09-16 01:36:16 -0300

    Nova versao v1 release3

--- src/config.js
+++ src/config.js
@@ -1,20 +1,3 @@
-// src/config.js
-let _ticketPriceCents = Number(process.env.PRICE_CENTS || 5500);
-
-export function getTicketPriceCents() {
-  return _ticketPriceCents;
-}
-
-export function setTicketPriceCents(v) {
-  const n = Number(v);
-  if (!Number.isFinite(n) || n <= 0) {
-    throw new Error("invalid_price");
-  }
-  _ticketPriceCents = Math.round(n);
-  console.log("[config] ticket price set to", _ticketPriceCents, "cents");
-  return _ticketPriceCents;
-}
-
 export {
   ensureAppConfig,
   getTicketPriceCents,
